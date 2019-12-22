a01a87577bacc371a3ff76abba6956416812aa18
Mike Zanercik <Michael.zanercik@gmail.com>
2019-12-22 14:25:27 -0500

    deploying

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const connectionString = process.env.JAWSDB_URL || {
   host: "localhost",
   port: 3306,
   user: "root",
   password: "rootroot",
   database: "burgers_db"
-});
+}
+
+
+const connection = mysql.createConnection(connectionString);
 
 // Make connection.
 connection.connect(function(err) {
